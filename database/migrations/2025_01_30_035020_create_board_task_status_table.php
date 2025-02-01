49b49267dd8a0a0e4805c5365e881d65f1448e15
Jose Ricardo <josericardojunior@gmail.com>
2025-02-01 16:46:17 -0300

    Initial migrations

---
+++ database/migrations/2025_01_30_035020_create_board_task_status_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('board_task_status', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('board_id')->constrained()->onDelete('cascade');
+            $table->foreignId('task_status_id')->constrained()->onDelete('cascade');
+            $table->unsignedInteger('sort_order');
+            $table->unique(['board_id', 'task_status_id']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('board_task_status');
+    }
+};
