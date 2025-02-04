a545d0e7bdca5b4777e5ee8134b21135baa2a2e4
Jose Ricardo <josericardojunior@gmail.com>
2025-02-04 02:09:55 -0300

    Fix hot reload

--- vite.config.js
+++ vite.config.js
@@ -9,11 +9,26 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.ts'],
             refresh: true,
         }),
-        vue(),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
     ],
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './resources/js')
         },
+    },
+    server: {
+        hmr: {
+            host: '127.0.0.1'
+        },
+        watch: {
+            usePolling: true
+        }
     },
 }); 
