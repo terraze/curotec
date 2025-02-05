367a0df14fec0bd8d7c5cb198731de95511cf1e1
Jose Ricardo <josericardojunior@gmail.com>
2025-02-04 23:42:17 -0300

    Docker configuration to auto run reverb on start

--- resources/js/components/YourComponent.js
+++
@@ -1,8 +0,0 @@
-mounted() {
-    window.Echo.channel('tasks')
-        .listen('TaskUpdated', (e) => {
-            // Handle the update here
-            console.log('Task updated:', e.task);
-            // Update your component's state
-        });
-} 
